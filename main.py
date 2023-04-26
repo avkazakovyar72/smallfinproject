9108655e8ee007c0b07734a51ada80059405ce80
aleksey <av_kazakov_yar@mail.ru>
2023-04-26 23:55:32 +0300

    Внесены дополнения по окнам и функциям

--- main.py
+++ main.py
@@ -3,6 +3,37 @@ import tkinter as tk
 class Main(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
+        self.init_main()
+
+    def init_main(self):
+        toolbar = tk.Frame(bg='#d7d8e0', bd= 2)
+        toolbar.pack(side=tk.TOP, fill= tk.X)
+
+        self.add_img = tk.PhotoImage(file= '../icons/icons8-папка.gif')
+        button_open_dialog = tk.Button(toolbar, text='Добавить позицию', command=self.open_dialog, bg='#d7d8e0',
+                                       bd=0, compound=tk.TOP, image=self.add_img)
+        button_open_dialog.pack(side=tk.LEFT)
+
+    def open_dialog(self):
+        Child()
+
+
+class Child (tk.Toplevel):
+    def __int__(self):
+        super.__init__(root)
+        self.init_child()
+
+    def init_child(self):
+        self.title('Добавить доходы/расходы')
+        self.geometry('400x300+400+300')
+        self.resizable(False, False)
+
+        self.grab_set()
+        self.focus_set()
+
+
+
+
 
 
 if __name__ == '__main__':
